c9bf4793b2d739732f0d35f7eafbf56faabe7c66
Ozair Zaman <48018195+OzairZaman@users.noreply.github.com>
2019-08-14 16:17:11 +1000

    initial

---
+++ Assets/Scripts/Enteties/Bomb.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mirror;
+
+public class Bomb : NetworkBehaviour
+{
+    public float explosionRadius = 2f;
+    public float explodeDelay = 2f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+
+    void Start()
+    {
+        StartCoroutine(Explode());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(explodeDelay);
+        CmdExplode(transform.position, explosionRadius);
+        
+    }
+
+    [Command]
+    void CmdExplode(Vector3 position, float radius)
+    {
+        Collider[] hits = Physics.OverlapSphere(position, radius);
+        foreach (var hit in hits)
+        {
+            NetworkServer.Destroy(hit.gameObject);
+        }
+
+        NetworkServer.Destroy(gameObject);
+    }
+}
