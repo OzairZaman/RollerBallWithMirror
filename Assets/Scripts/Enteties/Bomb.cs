327d3505a0860346b89565c4e8eb9c65289a3133
Ozair Zaman <48018195+OzairZaman@users.noreply.github.com>
2019-08-21 16:01:30 +1000

    added explosion additions
    new particle system

--- Assets/Scripts/Enteties/Bomb.cs
+++ Assets/Scripts/Enteties/Bomb.cs
@@ -2,35 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.Events;
+
 using Mirror;
 
 public class Bomb : NetworkBehaviour
 {
     public float explosionRadius = 2f;
     public float explodeDelay = 2f;
+    public float destroyDelay = 1f;
+
+    public GameObject linePrefab;
+
+    private Animation anim;
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
-
+    void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
     void Start()
     {
         StartCoroutine(Explode());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public IEnumerator Explode()
     {
         yield return new WaitForSeconds(explodeDelay);
+
+        Explode(transform.position, explosionRadius);
         CmdExplode(transform.position, explosionRadius);
-        
+
+        anim.Play("Shrink");
+
+        yield return new WaitForSeconds(destroyDelay);
+
+        NetworkServer.Destroy(gameObject);
+
+    }
+
+    void CreateLine(Vector3 start, Vector3 end)
+    {
+        GameObject clone = Instantiate(linePrefab, transform);
+        LineRenderer line = clone.GetComponent<LineRenderer>();
+        line.positionCount = 2;
+        line.SetPosition(0, start);
+        line.SetPosition(1, end);
     }
 
     [Command]
@@ -43,5 +65,18 @@ public class Bomb : NetworkBehaviour
         }
 
         NetworkServer.Destroy(gameObject);
+    }
+
+    void Explode(Vector3 position, float radius)
+    {
+        Collider[] hits = Physics.OverlapSphere(position, radius);
+        foreach (var hit in hits)
+        {
+            NetworkIdentity networkId = hit.GetComponent<NetworkIdentity>();
+            if (networkId && hit.name.Contains("Enemy"))
+            {
+                CreateLine(transform.position, hit.transform.position);
+            }
+        }
     }
 }
